b0f396dc2bcd430954c6d5ea0c0d131b2a761339
Huy Bui <buiquanghuy.it@gmail.com>
2019-02-13 23:59:36 +0700

    Add Swagger.

---
+++ WebApiTest/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password)) {
+                return BadRequest("Wrong credential.");
+            }
+
+            return Ok("Successfull.");
+        }
+    }
+}
